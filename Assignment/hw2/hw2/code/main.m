ce64ac64b14f5f4714f417ca9db781f35d6a4e28
Avinash <ee12b1014@iith.ac.in>
2017-10-11 13:13:40 -0400

    First Commit

---
+++ Assignment/hw2/hw2/code/main.m
@@ -0,0 +1,7 @@
+dirList = dir('../data/*.jpg');
+list = {dirList.name}';
+% folder = dirList.folder;
+% fList = cell(length(list),1);
+% for i=1:length(list)
+%     fList{i} = fullfile(folder,list{i});
+% end
